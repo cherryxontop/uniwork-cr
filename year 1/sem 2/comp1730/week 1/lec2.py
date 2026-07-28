8df869503a74e65d9c8d899f050631614c88413f
cherryxontop <ramnanichhaya@gmail.com>
2026-07-28 20:53:55 +1000

    fuck i forgot to remove the docstrings

--- year 1/sem 2/comp1730/week 1/lec2.py
+++ year 1/sem 2/comp1730/week 1/lec2.py
@@ -1,6 +1,5 @@
-"""
-#ex 1
-
+#---------------------
+# #ex 1
 r = 5
 pi = 3.14
 
@@ -8,7 +7,7 @@ v = 4/3*pi*r**2
 print(f"volume of sphere =", v)
 print("volume of spehere with radius: " + str(r) + " is " + str(v))
 
-
+#---------------------
 # ex 2
 print("what are you doing?")
 task = input()
@@ -29,6 +28,7 @@ progress_bar = '#' * int(filled) + '.' * int(unfilled)
 print("loading " + task + " ...: " + percent)
 print("["+progress_bar+"] " + percent + "%")
 
+#---------------------
 # ex 3: importing libraries
 import math
 print("pi= ", math.pi)
@@ -36,9 +36,9 @@ print("pi= ", math.pi)
 # but i like numpy so
 import numpy as np
 print("pi= ", np.pi)
-"""
 
 
+#---------------------
 # ex 4: defining functions
 def change_in_percent(old, new):
     diff = new - old
