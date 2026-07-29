de4944be4c471e6bf7d29fad4d929962a984461c
cherryxontop <ramnanichhaya@gmail.com>
2026-07-30 00:55:58 +1000

    3 vblsgit add .

--- year 1/sem 2/phys1201/week 1/pd.py
+++ year 1/sem 2/phys1201/week 1/pd.py
@@ -19,3 +19,17 @@ g_total = g.subs(y, x**2)
 dg_dx_total = sp.diff(g_total, x)
 
 print("dg/dx =", dg_dx_total)
+
+
+#3 variables
+x, y, z = sp.symbols('x y z')
+g = (x**2) * y * (z**3)
+
+delg_dx = sp.diff(g, z)
+print("delg/dx =", delg_dx)
+
+delg_dy = sp.diff(g, y)
+print("delg/dy =", delg_dy)
+
+delg_dz = sp.diff(g, z)
+print("delg/dx =", delg_dz)
