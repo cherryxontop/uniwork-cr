25acc15393c5297f075dc44166418aa2d688179a
cherryxontop <ramnanichhaya@gmail.com>
2026-07-29 22:47:48 +1000

    sympy is cool

---
+++ year 1/sem 2/phys1201/week 1/pd.py
@@ -0,0 +1,21 @@
+import sympy as sp
+
+x, y = sp.symbols('x y')
+
+g = (x**2) * y
+
+delg_dx = sp.diff(g, x)
+print("delg/dx =", delg_dx)
+
+delg_dy = sp.diff(g, y)
+print("delg/dy =", delg_dy)
+
+# computing total
+x, y = sp.symbols('x y')
+g = (x**2) * y
+
+g_total = g.subs(y, x**2)
+
+dg_dx_total = sp.diff(g_total, x)
+
+print("dg/dx =", dg_dx_total)
