805bcc7231eee7676a6970635ed55d125f0bc907
cherryxontop <ramnanichhaya@gmail.com>
2026-08-11 00:00:54 +1000

    donedonedoneeeddone

---
+++ year 1/sem 2/comp1730/week 3/lec1.py
@@ -0,0 +1,15 @@
+"""
+ITERATION
+iteration is the ability to repeatedly run a block a statements in a controlled manner- whe to start/stop/repeat
+    `while` loops
+        repeats a block of statements as long as a condition remains true.
+        useful for looping an indeterminate number of times, until a condition is satisfied
+    `for` loops
+        iterates through the elements of a collection or sequence
+        useful for looping a defined number of times
+    `break` to exit a loop
+    `continue` to go around again
+    `pass` to do nothing
+
+
+"""
